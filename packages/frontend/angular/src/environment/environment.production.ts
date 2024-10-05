f99cc176caace5ba632260c6c3a058aa2bac396f
bernous <bernous@gmail.com>
2024-10-05 21:32:50 +0100

    feat:Integrate serve-ssr and server builders for SSR

---
+++ packages/frontend/angular/src/environment/environment.production.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  env: 'production',
+  browserApiEndpoint: 'https://yourdomainname.com/api/',
+  serverApiEndpoint: 'https://yourdomainname.com/api/',
+  httpEndpoint: 'https://yourdomainname.com',
+  frontEndpoint: 'https://yourdomainname.com',
+  websocketEndpoint: 'wss://yourdomainname.com',
+};
---
+++ packages/frontend/angular/src/environment/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  env: 'development',
+  browserApiEndpoint: 'http://127.0.0.1:8000/api/',
+  serverApiEndpoint: 'http://127.0.0.1:8000/api/',
+  httpEndpoint: 'http://127.0.0.1:8000',
+  frontEndpoint: 'http://127.0.0.1:4200',
+  websocketEndpoint: 'ws://127.0.0.1:8001',
+};
